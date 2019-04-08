aa2193d4558e3cd2b1db033c5dd04ae5a939ff79
h-haraguchi <account.cgtdnpck@me.com>
2019-04-09 00:14:01 +0900

    support redux at minimum

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,11 @@
 
 import React, {Component} from "react";
 import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Action, createStore, Reducer} from "redux";
+import {Provider} from "react-redux";
+
+const reducer: Reducer = (state = {}, action: Action) => state;
+const store = createStore(reducer);
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -19,11 +24,13 @@ type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
+      <Provider store={store}>
+        <View style={styles.container}>
+          <Text style={styles.welcome}>Welcome to React Native!</Text>
+          <Text style={styles.instructions}>To get started, edit App.js</Text>
+          <Text style={styles.instructions}>{instructions}</Text>
+        </View>
+      </Provider>
     );
   }
 }
