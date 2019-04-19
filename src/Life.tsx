07b356337288513adff60b21975c8b9d942085d7
h-haraguchi <account.cgtdnpck@me.com>
2019-04-19 22:02:28 +0900

    add health to state

--- src/Life.tsx
+++ src/Life.tsx
@@ -4,35 +4,51 @@ import {Action, Dispatch, Reducer} from "redux";
 import {connect} from "react-redux";
 
 type Props = State & Events
-type State = { status: 'たまご' | 'いきてる' | 'しんでる' }
+type State = {
+  status: 'たまご' | 'いきてる' | 'しんでる'
+  health: '健康' | '不健康'
+}
 type Events = {
   takeCare(): void
   notTakeCare(): void
 }
 
-export const reducer: Reducer = (state: State = {status: 'たまご'}, action: Action) => {
+const initialState: State = {
+  status: 'たまご',
+  health: '健康'
+};
+
+export const reducer: Reducer = (state: State = initialState, action: Action) => {
   switch (action.type) {
     case 'TAKE_CARE':
       switch (state.status) {
         case 'たまご':
-          return {status: 'いきてる'};
+          return {...state, status: 'いきてる'};
         case 'いきてる':
-          return {status: 'いきてる'};
+          return {...state, status: 'いきてる'};
         case 'しんでる':
-          return {status: 'しんでる'};
+          return {...state, status: 'しんでる'};
         default:
           return {...state}
       }
     case 'NOT_TAKE_CARE':
-      switch (state.status) {
-        case 'たまご':
-          return {status: 'しんでる'};
-        case 'いきてる':
-          return {status: 'しんでる'};
-        case "しんでる":
-          return {status: 'しんでる'};
-        default:
+      if (Math.random() > 0.5) {
+        if (state.health === '健康') {
+          return {...state, health: '不健康'};
+        } else {
           return {...state}
+        }
+      } else {
+        switch (state.status) {
+          case 'たまご':
+            return {...state, status: 'しんでる'};
+          case 'いきてる':
+            return {...state, status: 'しんでる'};
+          case "しんでる":
+            return {...state, status: 'しんでる'};
+          default:
+            return {...state}
+        }
       }
     default:
       return {...state}
@@ -42,6 +58,7 @@ export const reducer: Reducer = (state: State = {status: 'たまご'}, action: A
 const Life: FC<Props> = props => (
   <View>
     <Text>{props.status}</Text>
+    <Text>{props.health}</Text>
     <Button title={'世話をする'} onPress={props.takeCare}/>
     <Button title={'世話をしない'} onPress={props.notTakeCare}/>
   </View>
