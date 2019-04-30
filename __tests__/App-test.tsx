907738be79a162c811d49dd909d240dd5b0019cb
tronperidot <tronperidot@gmail.com>
2019-04-30 18:01:00 +0900

    fix test.

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -4,6 +4,22 @@ import renderer from 'react-test-renderer';
 import * as React from "react";
 import App from "../src/App";
 
+jest.mock('react-navigation', () => {
+  return {
+    createAppContainer: jest.fn().mockReturnValue(function NavigationContainer(props) {return null;}),
+    createDrawerNavigator: jest.fn(),
+    createMaterialTopTabNavigator: jest.fn(),
+    createStackNavigator: jest.fn(),
+    StackActions: {
+        push: jest.fn().mockImplementation(x => ({...x,  "type": "Navigation/PUSH"})),
+        replace: jest.fn().mockImplementation(x => ({...x,  "type": "Navigation/REPLACE"})),
+    },
+    NavigationActions: {
+        navigate: jest.fn().mockImplementation(x => x),
+    }
+  }
+});
+
 it('renders correctly', () => {
   renderer.create(<App />);
 });
