7c7b68a3a10f8f8d99682a8b79d96b5f6828b4d5
h-haraguchi <account.cgtdnpck@me.com>
2019-05-06 02:09:59 +0900

    generate snapshot testing

--- __tests__/main/reducer-test.ts
+++ __tests__/main/reducer-test.ts
@@ -1,44 +1,20 @@
 import { main, Situation, Stage } from "../../src/main/reducer";
 import { breakUp, loseJob, makeWorry, neglect } from "../../src/main/actions";
+import { takeSnapshot } from "../utils"
 import { MainState } from "../../src/main/interface"
+import { AnyAction } from "redux"
 
-const statePatterns: (MainState | undefined)[] = [
-  undefined,
-  { stage: Stage.NORMAL, situation: Situation.EMPTY },
-  { stage: Stage.NORMAL, situation: Situation.NO_JOB },
-  { stage: Stage.NORMAL, situation: Situation.LINE },
-  { stage: Stage.DEAD, situation: Situation.EMPTY },
-  { stage: Stage.BROKEN_UP, situation: Situation.EMPTY }
-];
-
-describe('breakUp', () => {
-  statePatterns.forEach(state => {
-    it(`state: ${JSON.stringify(state)}`, () => {
-      expect(main(state, breakUp())).toMatchSnapshot()
-    })
+describe('main', () => {
+  takeSnapshot<MainState, AnyAction>({
+    reducer: main,
+    states: [
+      undefined,
+      { stage: Stage.NORMAL, situation: Situation.EMPTY },
+      { stage: Stage.NORMAL, situation: Situation.NO_JOB },
+      { stage: Stage.NORMAL, situation: Situation.LINE },
+      { stage: Stage.DEAD, situation: Situation.EMPTY },
+      { stage: Stage.BROKEN_UP, situation: Situation.EMPTY }
+    ],
+    actionCreators: [breakUp, neglect, loseJob, makeWorry]
   });
-})
-
-describe('neglect', () => {
-  statePatterns.forEach(state => {
-    it(`state: ${JSON.stringify(state)}`, () => {
-      expect(main(state, neglect())).toMatchSnapshot()
-    })
-  });
-})
-
-describe('loseJob', () => {
-  statePatterns.forEach(state => {
-    it(`state: ${JSON.stringify(state)}`, () => {
-      expect(main(state, loseJob())).toMatchSnapshot()
-    })
-  });
-})
-
-describe('makeWorry', () => {
-  statePatterns.forEach(state => {
-    it(`state: ${JSON.stringify(state)}`, () => {
-      expect(main(state, makeWorry())).toMatchSnapshot()
-    })
-  });
-})
+});
