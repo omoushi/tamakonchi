d001784763b3fb73b89e0d0b91017f71c805acbe
tronperidot <tronperidot@gmail.com>
2019-06-15 13:53:08 +0900

    #39 add pager component test.

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -3,27 +3,10 @@ import 'react-native';
 import renderer from 'react-test-renderer';
 import * as React from "react";
 import App from "../src/App";
-import {
-  NavigationContainer,
-  NavigationNavigateAction,
-  NavigationPushAction,
-  NavigationReplaceAction
-} from "react-navigation";
 
-jest.mock('react-navigation', (): object => {
-  return {
-    createAppContainer: jest.fn().mockReturnValue((): NavigationContainer | null => {return null;}),
-    createDrawerNavigator: jest.fn(),
-    createMaterialTopTabNavigator: jest.fn(),
-    createStackNavigator: jest.fn(),
-    StackActions: {
-      push: jest.fn().mockImplementation((x): NavigationPushAction => ({...x,  "type": "Navigation/PUSH"})),
-      replace: jest.fn().mockImplementation((x): NavigationReplaceAction => ({...x,  "type": "Navigation/REPLACE"})),
-    },
-    NavigationActions: {
-      navigate: jest.fn().mockImplementation((x): NavigationNavigateAction => x),
-    }
-  };
+jest.mock('../src/Pager', () => {
+  const pager = () => <div />;
+  return pager;
 });
 
 it('renders correctly', (): void => {
---
+++ __tests__/Pager-test.tsx
@@ -0,0 +1,37 @@
+import 'react-native';
+// Note: test renderer must be required after react-native.
+import renderer from 'react-test-renderer';
+import * as React from "react";
+import reducer from "../src/reducers";
+import Pager from  '../src/Pager';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+import {
+  NavigationContainer,
+  NavigationNavigateAction,
+  NavigationPushAction,
+  NavigationReplaceAction
+} from "react-navigation";
+
+jest.mock('react-navigation', (): object => {
+  return {
+    createAppContainer: jest.fn().mockReturnValue((): NavigationContainer | null => {return null;}),
+    createDrawerNavigator: jest.fn(),
+    createMaterialTopTabNavigator: jest.fn(),
+    createStackNavigator: jest.fn(),
+    StackActions: {
+      push: jest.fn().mockImplementation((x): NavigationPushAction => ({...x,  "type": "Navigation/PUSH"})),
+      replace: jest.fn().mockImplementation((x): NavigationReplaceAction => ({...x,  "type": "Navigation/REPLACE"})),
+    },
+    NavigationActions: {
+      navigate: jest.fn().mockImplementation((x): NavigationNavigateAction => x),
+    }
+  };
+});
+
+it('renders correctly', (): void => {
+  const store = createStore(reducer);
+  const component = renderer.create(<Provider store={store}><Pager/></Provider>);
+  component.unmount();
+});
